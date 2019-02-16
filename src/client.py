fd2820bd90394c0224892b19e199b59ab002cbd9
Seth Giovanetti <gio@utdallas.edu>
2019-02-15 20:52:33 -0600

    Python scaffolding

---
+++ src/client.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+"""Summary
+# Description: 
+Client module for ServerBasedChat
+
+# Authors:
+- Seth Giovanetti
+
+
+"""
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
